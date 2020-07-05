f141bf5e26b3e00e39213062cb09ce8a5ed80c18
Farooq Alaulddin <37892823+FarooqAlaulddin@users.noreply.github.com>
2020-07-05 01:49:36 -0700

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,15 @@
+class Node{
+        
+	// a simple node class
+	    int key;
+        int height;
+        Node l;
+        Node r;
+
+        public Node(int data)
+        {
+            key = data;
+            height = 1;
+
+        }
+}
